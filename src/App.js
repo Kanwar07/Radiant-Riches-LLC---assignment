ac1d6411db2f01aa6d915abc98343fe8b139c895
Kanwar07 <anmolkanwar07@gmail.com>
2024-02-27 19:48:59 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import WebsitePage from "./components/main/WebsitePage";
-import Contextdata from "./components/context/Context";
 
 function App() {
   return (
-    <Contextdata class="bg-[#fbfcfd]">
+    <div class="bg-[#fbfcfd]">
       <Navbar />
       <WebsitePage />
-    </Contextdata>
+    </div>
   );
 }
 
