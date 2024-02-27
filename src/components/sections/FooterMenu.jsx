eaabcae5a549e32d350a3f754cd51b930dc0b3f6
Kanwar07 <anmolkanwar07@gmail.com>
2024-02-27 23:50:25 +0530

    list0fCountries

--- src/components/sections/FooterMenu.jsx
+++ src/components/sections/FooterMenu.jsx
@@ -3,12 +3,12 @@ import menu1 from "../data/Menu1.json";
 import menu2 from "../data/Menu2.json";
 import CategoryButton from "../button/CategoryButton";
 import ContactButton from "../button/ContactButton";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import countries from "../data/Countries.json";
 
 function FooterMenu() {
   return (
     <div class="flex pr-[20%] pl-[20%] h-[40%h] w-full bg-[#212731] flex justify-around pt-16 max-lg:flex-col max-xl:flex-row">
-      <div class="flex flex-col">
+      <div class="flex flex-col max-lg:mb-8">
         <div class="font-inter text-[16px] font-normal leading-[1.5rem] tracking-normal text-left text-[#ffffff] pb-4">
           Categories
         </div>
@@ -23,7 +23,7 @@ function FooterMenu() {
           })}
         </div>
       </div>
-      <div>
+      <div class="max-lg:mb-8">
         <div class="font-inter text-[16px] font-normal leading-[1.5rem] tracking-normal text-left text-[#ffffff] pb-4">
           Contact
         </div>
@@ -38,11 +38,21 @@ function FooterMenu() {
           })}
         </div>
       </div>
-      <div class="flex flex-row text-[#ffffff] max-lg:justify-start max-2xl:justify-center pt-16 pb-16">
-        <div>
-          <KeyboardArrowDownIcon />
-        </div>
-        <div>United States</div>
+      <div class="flex flex-row text-[#ffffff] max-lg:justify-start max-2xl:justify-center pl-4 mt-16 mb-32">
+        <select class="text-[#ffffff] bg-transparent w-60">
+          {countries.map((country) => {
+            const { name, code } = country;
+            return (
+              <option
+                key={code}
+                value={name}
+                class="text-[#ffffff] bg-[#000000]"
+              >
+                {name}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </div>
   );
